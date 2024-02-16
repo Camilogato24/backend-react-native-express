b9fef225c9e383a3ef89e6aaeafddf7818967394
Camilo Fernandez <Camilogato24@users.noreply.github.com>
2024-02-15 22:42:32 -0500

    generate token rest guard token in some services

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 import { User } from "../models/models";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const login = async (req, res) => {
   const { password, username } = req.body;
@@ -16,14 +17,14 @@ export const login = async (req, res) => {
         msg: `Has olvidado tu contraseña?, contraseña incorrecta. `
       })
     }
+
+    const token = jwt.sign({ username: username }, 'VQ54u361L!aJnIKoY*S', { expiresIn: '1h' });
    
-    res.status(200).json({
-      msg: `Usuario ${username} registrado exitosamente`
-    })
+    res.status(200).json({token})
   } catch (error) {
     console.log(error)
     res.status(500).json({
-      msg: `Ha ocurrido un problema al crear el usuario`,
+      msg: `Ha ocurrido un problema al iniciar sesión`,
       error
     })
   }
