2ff9908226fc53894c77e203561b3dd0e695fbb8
Camilo Fernandez <Camilogato24@users.noreply.github.com>
2024-02-18 16:44:23 -0500

    complet

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ import morgan from "morgan";
 
 const app = express();
 app.use(express.json());
-app.use(tasksRouter);
 app.use(morgan("dev"))
 app.use(cors());
+app.use(tasksRouter);
 app.use(authRouter);
 export default app;
 
